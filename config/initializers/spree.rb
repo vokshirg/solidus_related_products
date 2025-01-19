c7067b69e7caae785917a1abefef981a7148e492
Ivan Grishkov <ivan@pine-code.com>
2025-01-19 03:27:03 +0100

    solidus 4.4 syntax with menu childrens

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Spree::Backend::Config.configure do |config|
-  break unless config.respond_to?(:menu_items)
+  products_menu = config.menu_items.detect { |item| item.label == :products }
 
-  config.menu_items.detect { |menu_item| menu_item.label == :products }&.tap do |menu_item|
-    menu_item.label = :relation_types
-  end
+  products_menu.children << Spree::BackendConfiguration::MenuItem.new(
+    label: :relation_types,
+    condition: -> { can?(:admin, Spree::RelationType) },
+    url: :admin_relation_types_path,
+    match_path: '/relation_types'
+  )
 end
