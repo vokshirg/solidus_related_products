19e4d946579504aca93b9a61da76197dfb3017ae
Ivan Grishkov <ivan@pine-code.com>
2025-01-19 02:52:28 +0100

    new syntax for menu items

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Spree::Backend::Config.configure do |config|
-  break if !config.respond_to? :menu_items
+  break unless config.respond_to?(:menu_items)
 
-  config.menu_items.detect { |menu_item|
-    menu_item.label == :products
-  }.sections << :relation_types
+  config.menu_items.detect { |menu_item| menu_item.label == :products }&.tap do |menu_item|
+    menu_item.label = :relation_types
+  end
 end
